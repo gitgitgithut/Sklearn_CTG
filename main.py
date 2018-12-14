55e5456f0485f5e7e2472e514e8477c35f6e072f
Lin <knightlin1216@hotmail.com>
2018-12-14 01:24:03 -0500

    updated

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from sklearn.preprocessing import label_binarize
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.neural_network import MLPClassifier
 from itertools import cycle
-from scipy import interp
 from os import makedirs, path
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,7 +18,7 @@ import csv
 
 #################### Preprocessing #################################
 def dimred(data, target):
-    pca = PCA(n_components=4)
+    pca = PCA(n_components=11)
     return pca.fit_transform(data, target)
 
 #################### ML Training ###################################
@@ -32,62 +31,39 @@ def test(clf, name, data, target, e = 0):
     else:
         fname = name + str(e) + '.csv'
     target = label_binarize(target, classes=[1, 2, 3])
-    n_classes = target.shape[1]
     clf = OneVsRestClassifier(clf)
     with open(name + '/' + fname, 'w') as file:
         writer = csv.writer(file, delimiter=',')
         writer.writerow(["K", 'Precision', 'Recall'])
         for k in [2, 5, 10, 100, 500, 1000, 2126]:
-############################################################################
             predict = cross_val_predict(clf, data, target, cv=k)
             writer.writerow([k, precision_score(target, predict, average='micro'),
                              recall_score(target, predict, average='micro')])
-            fpr = dict()
-            tpr = dict()
-            roc_auc = dict()
-            for i in range(n_classes):
-                fpr[i], tpr[i], _ = roc_curve(target[:, i], predict[:, i])
-                roc_auc[i] = auc(fpr[i], tpr[i])
-            fpr["micro"], tpr["micro"], _ = roc_curve(target.ravel(), predict.ravel())
-            roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
-            lw = 2
-            all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
-            mean_tpr = np.zeros_like(all_fpr)
-            for i in range(n_classes):
-                mean_tpr += interp(all_fpr, fpr[i], tpr[i])
-            mean_tpr /= n_classes
-            fpr["macro"] = all_fpr
-            tpr["macro"] = mean_tpr
-            roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
-
-            # Plot all ROC curves
-            pname = fname[0:-4] + ' with ' + str(k) + "-fold cv"
-            fig = plt.figure()
-            colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
-            for i, color in zip(range(n_classes), colors):
-                plt.plot(fpr[i], tpr[i], color=color, lw=lw,
-                         label='ROC curve of class {0} (area = {1:0.2f})'
-                               ''.format(i+1, roc_auc[i]))
-            plt.plot([0, 1], [0, 1], 'k--', lw=lw)
-            plt.xlim([0.0, 1.0])
-            plt.ylim([0.0, 1.05])
-            plt.xlabel('False Positive Rate')
-            plt.ylabel('True Positive Rate')
-            plt.title(pname)
-            plt.legend(loc="lower right")
-            plt.savefig(name + '/' + pname + '.png')
-            plt.clf()
-            plt.close(fig)
-#######################################################################################
-            # score = cross_validate(clf, data, target, cv=k, scoring=scoring)
-            # avg_p, avg_r = 0, 0
-            # for i in score['test_precision_micro']:
-            #     avg_p = avg_p + i
-            # for j in score['test_recall_micro']:
-            #     avg_r = avg_r + j
-            # avg_p = avg_p / k
-            # avg_r = avg_r / k
-            # writer.writerow([avg_p, avg_r])
+            ##### from https://scikit-learn.org/stable/auto_examples/model_selection/plot_roc.html#######
+            # n_classes = target.shape[1]
+            # fpr = dict()
+            # tpr = dict()
+            # roc_auc = dict()
+            # for i in range(n_classes):
+            #     fpr[i], tpr[i], _ = roc_curve(target[:, i], predict[:, i], drop_intermediate=False)
+            #     roc_auc[i] = auc(fpr[i], tpr[i])
+            # pname = fname[0:-4] + ' with ' + str(k) + "-fold cv"
+            # fig = plt.figure()
+            # colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+            # for i, color in zip(range(n_classes), colors):
+            #     plt.plot(fpr[i], tpr[i], color=color, lw=2,
+            #              label='ROC curve of class {0} (area = {1:0.2f})'
+            #                    ''.format(i+1, roc_auc[i]))
+            # plt.plot([0, 1], [0, 1], 'k--', lw=2)
+            # plt.xlim([0.0, 1.0])
+            # plt.ylim([0.0, 1.05])
+            # plt.xlabel('False Positive Rate')
+            # plt.ylabel('True Positive Rate')
+            # plt.title(pname)
+            # plt.legend(loc="lower right")
+            # plt.savefig(name + '/' + pname + '.png')
+            # plt.clf()
+            # plt.close(fig)
     print(fname, "done")
     return
 
@@ -113,14 +89,14 @@ def decisionTree(data, target):
 
 
 def randomForest(data, target):
-    for e in [30, 50, 70, 90]:
+    for e in [10, 30, 50, 70, 90, 110, 130, 150]:
         rf = RandomForestClassifier(n_estimators=e)
         threading.Thread(target=test, args=(rf, 'rf', data, target, e)).start()
     return
 
 
 def adaBoost(data, target):
-    for e in [30, 50, 70, 90]:
+    for e in [10, 30, 50, 70, 90, 110, 130, 150]:
         ada = AdaBoostClassifier(n_estimators=e)
         threading.Thread(target=test, args=(ada, 'ada', data, target, e)).start()
     return
@@ -131,12 +107,17 @@ def mlp(data, target):
     #        (MLPClassifier(solver='lbfgs', alpha=0.2, activation='logistic'), 'mlp, 0,2, logistic'),
     #        (MLPClassifier(solver='lbfgs', alpha=0.2, activation='tanh'), 'mlp, 0.2, tanh'),
     #        (MLPClassifier(solver='lbfgs', alpha=0.2, activation='relu'), 'mlp, 0.2, relu')]
-    lst = [(MLPClassifier(solver='lbfgs', alpha=0.0001, activation='logistic'), 'mlp,0.0001, logistic'),
-           (MLPClassifier(solver='lbfgs', alpha=0.001, activation='logistic'), 'mlp, 0,001, logistic'),
-           (MLPClassifier(solver='lbfgs', alpha=0.01, activation='logistic'), 'mlp, 0.01, logistic'),
-           (MLPClassifier(solver='lbfgs', alpha=0.0001, activation='tanh'), 'mlp,0.0001, tanh'),
-           (MLPClassifier(solver='lbfgs', alpha=0.001, activation='tanh'), 'mlp, 0,001, tanh'),
-           (MLPClassifier(solver='lbfgs', alpha=0.01, activation='tanh'), 'mlp, 0.01, tanh')]
+    # lst = [(MLPClassifier(solver='lbfgs', alpha=0.0001, activation='logistic'), 'mlp,0.0001, logistic'),
+    #        (MLPClassifier(solver='lbfgs', alpha=0.001, activation='logistic'), 'mlp, 0,001, logistic'),
+    #        (MLPClassifier(solver='lbfgs', alpha=0.01, activation='logistic'), 'mlp, 0.01, logistic'),
+    #        (MLPClassifier(solver='lbfgs', alpha=0.0001, activation='tanh'), 'mlp,0.0001, tanh'),
+    #        (MLPClassifier(solver='lbfgs', alpha=0.001, activation='tanh'), 'mlp, 0,001, tanh'),
+    #        (MLPClassifier(solver='lbfgs', alpha=0.01, activation='tanh'), 'mlp, 0.01, tanh')]
+    lst = [(MLPClassifier(solver='lbfgs', alpha=0.001, activation='logistic', hidden_layer_sizes=6), 'mlp, 0,001, logistic, 6 hidden layers'),
+           (MLPClassifier(solver='lbfgs', alpha=0.001, activation='logistic', hidden_layer_sizes=7),'mlp, 0,001, logistic, 7 hidden layers'),
+           (MLPClassifier(solver='lbfgs', alpha=0.001, activation='logistic', hidden_layer_sizes=8), 'mlp, 0,001, logistic, 8 hidden layers'),
+           (MLPClassifier(solver='lbfgs', alpha=0.001, activation='logistic', hidden_layer_sizes=9), 'mlp, 0,001, logistic, 9 hidden layers'),
+           (MLPClassifier(solver='lbfgs', alpha=0.001, activation='logistic', hidden_layer_sizes=10), 'mlp, 0,001, logistic, 10 hidden layers')]
     for ann in lst:
         threading.Thread(target=test, args=(ann[0], ann[1], data, target)).start()
     return
@@ -145,13 +126,13 @@ def main():
     dataset = np.genfromtxt('CTG.csv', delimiter=',')
     # size = np.shape(dataset)
     target = dataset[2:, -1]
-    data = dataset[2:, 10:31]
-    # data = dimred(dataset[2:size[0]-1, 10:31], target)
+    # data = dataset[2:, 10:31]
+    data = dimred(dataset[2:, 10:31], target)
     # threading.Thread(target=regression, args=(data, target)).start()
     # threading.Thread(target=decisionTree, args=(data, target)).start()
     # threading.Thread(target=randomForest, args=(data, target)).start()
     # threading.Thread(target=adaBoost, args=(data, target)).start()
-    # threading.Thread(target=mlp, args=(data, target)).start()
+    threading.Thread(target=mlp, args=(data, target)).start()
     return
 
 ####################################################################################
